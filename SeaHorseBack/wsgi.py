9115e97e05513a5c71ff2d3a0077092682334da4
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-05 18:05:15 -0400

    again

--- SeaHorseBack/wsgi.py
+++ SeaHorseBack/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seahorsejetski.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SeaHorseBack.settings")
 
 application = get_wsgi_application()
